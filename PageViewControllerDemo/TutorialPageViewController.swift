c326b9a9323ff67cb24ff07712bdd21317d2ea3e
Jeff Norton <jeff12ntn@gmail.com>
2016-12-07 12:16:33 -0700

    Storyboard setup

---
+++ PageViewControllerDemo/TutorialPageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TutorialPageViewController.swift
+//  PageViewControllerDemo
+//
+//  Created by Jeff Norton on 12/7/16.
+//  Copyright © 2016 JeffCryst. All rights reserved.
+//
+
+import UIKit
+
+class TutorialPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
